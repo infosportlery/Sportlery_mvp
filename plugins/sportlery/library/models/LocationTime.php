aeb4ef4c04f8019c59e314f228fc4cb1d32b8841
Stidges <info@stidges.com>
2017-04-11 12:54:39 +0200

    Show days without times as closed

--- plugins/sportlery/library/models/LocationTime.php
+++ plugins/sportlery/library/models/LocationTime.php
@@ -25,4 +25,21 @@ class LocationTime extends Model
      * @var string The database table used by the model.
      */
     public $table = 'spr_location_times';
+
+    public function filterFields($fields, $context = null)
+    {
+        if ($context === 'relation') {
+            $days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+            foreach ($days as $day) {
+                $start = "{$day}_start";
+                $end = "{$day}_end";
+                if (!$fields->$start->value || !$fields->$end->value) {
+                    $fields->$start->label = ucfirst($day);
+                    $fields->$start->type = 'text';
+                    $fields->$start->value = 'Closed';
+                    $fields->$end->hidden = true;
+                }
+            }
+        }
+    }
 }

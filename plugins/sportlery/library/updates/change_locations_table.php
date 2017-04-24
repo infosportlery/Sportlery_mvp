42ab7bda87c5ac186916f48ffe531fdbb2a1a0e3
Stidges <info@stidges.com>
2017-04-24 16:00:05 +0200

    Update updates

--- plugins/sportlery/library/updates/change_locations_table.php
+++ plugins/sportlery/library/updates/change_locations_table.php
@@ -13,13 +13,10 @@ class ChangeLocationsTable extends Migration
     {
         $emails = DB::table('spr_locations')->lists('email', 'id');
         Schema::table('spr_locations', function(Blueprint $table) {
-            $table->dropColumn('email', 'kvk_number', 'iban_id', 'btw_number', 'avatar');
+            $table->dropColumn('email', 'avatar');
         });
         Schema::table('spr_locations', function(Blueprint $table) {
             $table->string('email')->after('name')->nullable();
-            $table->string('kvk_number')->after('is_hidden')->nullable();
-            $table->string('btw_number')->after('kvk_number')->nullable();
-            $table->string('iban_number')->after('btw_number')->nullable();
             $table->unsignedInteger('user_id')->after('btw_number')->nullable()->index();
         });
 
@@ -32,14 +29,11 @@ class ChangeLocationsTable extends Migration
     {
         $emails = DB::table('spr_locations')->lists('email', 'id');
         Schema::table('spr_locations', function(Blueprint $table) {
-            $table->dropColumn(['email', 'kvk_number', 'iban_number', 'btw_number', 'user_id']);
+            $table->dropColumn(['email', 'user_id']);
         });
         Schema::table('spr_locations', function(Blueprint $table) {
             $table->string('email')->after('name');
             $table->string('avatar')->after('description');
-            $table->string('kvk_number');
-            $table->string('btw_number');
-            $table->string('iban_number');
         });
         foreach ($emails as $id => $email) {
             $email = $email ?: '';
